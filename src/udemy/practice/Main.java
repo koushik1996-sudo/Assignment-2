6739a45c1d69ac70dca71b877cee6d8cd5dd149c
koushik1996-sudo <67244993+koushik1996-sudo@users.noreply.github.com>
2021-04-07 22:00:49 -0500

    Initial commit

---
+++ src/udemy/practice/Main.java
@@ -0,0 +1,16 @@
+package udemy.practice;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s = "this is a programming language string";
+        String[] token = s.split(" ");
+        for (int i = 0; i < token.length; i++) {
+            StringBuilder input = new StringBuilder();
+            input.append(token[i]);
+            input.reverse();
+            System.out.print(input + " ");
+
+        }
+    }
+}
